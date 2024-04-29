edc7006237f1867f2d53d3d427432bb179ab0ff5
GriseoVul <off.sergienko@gmail.com>
2024-04-29 23:17:31 +0300

    Reorganized folder structure

--- src/ReadBooru.API/obj/Debug/net8.0/ReadBooru.AssemblyInfo.cs
+++ src/ReadBooru.API/obj/Debug/net8.0/ReadBooru.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReadBooru")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c317dcae36c86549db32b01c39f150ee7f65981")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReadBooru")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReadBooru")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
